7cc9cfd3a370d08db16fcad4a04f83a7047c558c
Hanoch Griner <eugriner@gmail.com>
2015-03-22 21:28:20 -0300

    q21 without debug

--- Traversal.java
+++ Traversal.java
@@ -7,7 +7,6 @@ public class Traversal {
 	static final Ocean ocean = new Ocean(0);
 	static final int pathColor = 200;
 	static final int deadEndColor = 100;
-	static boolean foundNemo = false;
 	
 	static void slow(){
 		try {
@@ -21,6 +20,8 @@ public class Traversal {
 		
 	static void q21() {
 		
+		boolean foundNemo = false;
+
 		//list of all elements that we have cross
 		LinkedList<Cell> l = new LinkedList<Cell>();
 		
@@ -42,8 +43,7 @@ public class Traversal {
 		
 		//west,south,east,north
 
-		int j = 0;
-		while(j < 2 && !foundNemo) {
+		while(!foundNemo) {
 			fish = q.pop();
 			
 			neighbors = fish.neighbors();
@@ -65,11 +65,55 @@ public class Traversal {
 	}
 
 	static void q22() {
+		
+		boolean foundNemo = false;
+
+		//list of all elements that we have cross
+		LinkedList<Cell> l = new LinkedList<Cell>();
+		
+		//list of elements to process
+		LinkedList<Cell> q = new LinkedList<Cell>();
+		
+		//fish that walks around
+		Cell fish = new Cell(-1, -1);
+		
+		//cell to process
+		LinkedList<Cell> neighbors = new LinkedList<Cell>();
+		
+		//initial position
+		Cell s = new Cell(ocean.marlin.x, ocean.marlin.y);
+		
+		//add first element
+		l.add(s);
+		q.add(s);
+		
+		//west,south,east,north
+
+		while(!foundNemo) {
+			fish = q.pop();
+			
+			neighbors = fish.neighbors();
+			
+			for(int i = 0; i < 4 && !foundNemo; i++) {
+				Cell water = neighbors.get(i);
+				
+				if(ocean.isNemo(water)) {
+					foundNemo = true;
+					fish = water;
+					System.out.println("Nemo is at: " + fish.toString());
+				}
+				else if((!ocean.isWall(water) && !contains(l, water))) {
+					q.add(water);
+					l.add(water);
+				}
+			}
+		}
 	}
 
 	// convenções de marca: 1 (WEST), 2(SOUTH), 3(EAST), 4(NORTH)
 	static final int WEST = 1, SOUTH = 2, EAST = 3, NORTH = 4;
-
+	
+	
 	static void q23() {
 	}
 
@@ -91,7 +135,7 @@ public class Traversal {
 		return b;
 	}
 	public static void main(String[] args) {
-	    q21();
+	    q22();
 	}
 
 }	
